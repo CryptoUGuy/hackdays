09c80e9617480a3bb703d3ab0bf0c39813fa74ec
Edson Alcala <edson.alcala@coinbase.com>
2022-01-27 15:31:02 +0000

    claned tests

--- examples/test/erc721/MintableNFT.test.ts
+++ examples/test/erc721/MintableNFT.test.ts
@@ -1,25 +1,25 @@
 import { expect } from "chai";
 import { Signer } from "ethers";
-import {  ethers } from "hardhat";
+import { ethers } from "hardhat";
 
-import {MintableNFT, MintableNFT__factory } from '../../typechain'
+import { MintableNFT, MintableNFT__factory } from '../../typechain'
 
-describe("MintableNFT tests", () =>{
-  let instance: MintableNFT 
+describe("MintableNFT tests", () => {
+  let instance: MintableNFT
   let accounts: Signer[]
   let user1Address: string;
   let user2Address: string;
   let user1Signer: Signer
   let user2Signer: Signer
-  
-  beforeEach(async() => {
+
+  beforeEach(async () => {
     accounts = await ethers.getSigners();
     user1Address = await accounts[0].getAddress()
     user2Address = await accounts[1].getAddress()
 
     user1Signer = accounts[0]
     user2Signer = accounts[1]
-    
+
     const factory = new MintableNFT__factory(user1Signer)
 
     instance = await factory.deploy();
@@ -41,7 +41,7 @@ describe("MintableNFT tests", () =>{
 
     // create an instance connected to a different user
     const newInstanceForNonOwnerUser = instance.connect(user2Signer)
-    
+
     await expect(newInstanceForNonOwnerUser.safeMint(user2Address, tokenId))
       .to.be.revertedWith("Ownable: caller is not the owner")
   });
--- examples/test/erc721/SimpleNFT.test.ts
+++ examples/test/erc721/SimpleNFT.test.ts
@@ -1,14 +1,14 @@
 import { expect } from "chai";
 import { Signer } from "ethers";
-import { deployments, ethers } from "hardhat";
+import { ethers } from "hardhat";
 
-import {SimpleNFT, SimpleNFT__factory } from '../../typechain'
-import {ETHEREUM_ADDRESS_LENGTH } from "../../constants"
+import { SimpleNFT, SimpleNFT__factory } from '../../typechain'
+import { ETHEREUM_ADDRESS_LENGTH } from "../../constants"
 
 describe("SimpleNFT tests", () => {
-  let instance: SimpleNFT 
+  let instance: SimpleNFT
   let accounts: Signer[]
-  
+
   it("deploys correctly", async () => {
     accounts = await ethers.getSigners();
     const factory = new SimpleNFT__factory(accounts[0])
