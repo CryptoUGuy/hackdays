1b84c17abc0a917bf276ae60328819adafab7566
Edson Alcala <edson.alcala@coinbase.com>
2022-01-27 15:48:08 +0000

    updated nft contracts

--- examples/contracts/erc721/EnumerableNFT.sol
+++ examples/contracts/erc721/EnumerableNFT.sol
@@ -22,10 +22,11 @@ pragma solidity 0.8.4;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 import "hardhat/console.sol";
 
-contract EnumerableNFT is ERC721, Ownable {
+contract EnumerableNFT is ERC721, ERC721Enumerable {
     using Counters for Counters.Counter;
 
     string public constant BASE_TOKEN_URI =
@@ -43,5 +44,23 @@ contract EnumerableNFT is ERC721, Ownable {
 
     function _baseURI() internal view virtual override returns (string memory) {
         return BASE_TOKEN_URI;
+    }
+
+    // The following functions are overrides required by Solidity.
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
--- examples/contracts/erc721/MetadataNFT.sol
+++ examples/contracts/erc721/MetadataNFT.sol
@@ -28,11 +28,10 @@ pragma solidity 0.8.4;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 import "hardhat/console.sol";
 
-contract EnumerableNFT is ERC721, ERC721Enumerable {
+contract MetadataNFT is ERC721 {
     using Counters for Counters.Counter;
 
     string public constant BASE_TOKEN_URI =
@@ -40,7 +39,7 @@ contract EnumerableNFT is ERC721, ERC721Enumerable {
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("EnumerableNFT", "EnumerableNFT") {}
+    constructor() ERC721("MetadataNFT", "MetadataNFT") {}
 
     function safeMint(address to) public {
         uint256 tokenId = _tokenIdCounter.current();
@@ -50,23 +49,5 @@ contract EnumerableNFT is ERC721, ERC721Enumerable {
 
     function _baseURI() internal view virtual override returns (string memory) {
         return BASE_TOKEN_URI;
-    }
-
-    // The following functions are overrides required by Solidity.
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 tokenId
-    ) internal override(ERC721, ERC721Enumerable) {
-        super._beforeTokenTransfer(from, to, tokenId);
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
     }
 }
